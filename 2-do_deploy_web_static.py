51d3c0bc008132c8ce668ac2b19d307c883a62fa
aissap <jesus.jesus.core@gmail.com>
2024-02-04 16:44:06 -0800

    fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -20,13 +20,13 @@ def do_deploy(archive_path):
         archive_name = os.path.basename(archive_path)
         base_name = os.path.splitext(archive_name)[0]
         put(archive_path, '/tmp/')
-        sudo('mkdir -p /data/web_static/releases/{}/'.format(base_name))
+        run('mkdir -p /data/web_static/releases/{}/'.format(base_name))
         main = "/data/web_static/releases/{}".format(base_name)
-        sudo('tar -xzf /tmp/{} -C {}/'.format(archive_name, main))
-        sudo('rm /tmp/{}'.format(archive_name))
-        sudo('mv {}/web_static/* {}/'.format(main, main))
-        sudo('rm -rf /data/web_static/current')
-        sudo('ln -s {}/ "/data/web_static/current"'.format(main))
+        run('tar -xzf /tmp/{} -C {}/'.format(archive_name, main))
+        run('rm /tmp/{}'.format(archive_name))
+        run('mv {}/web_static/* {}/'.format(main, main))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}/ "/data/web_static/current"'.format(main))
         return True
 
     except:
