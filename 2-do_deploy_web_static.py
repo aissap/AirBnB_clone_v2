2e8472da22bc9ee4344d009b3c090793f2991d05
aissap <jesus.jesus.core@gmail.com>
2024-02-04 16:39:43 -0800

    fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,32 +1,33 @@
 #!/usr/bin/python3
 """ Distributes an archive to your web servers """
-from fabric.api import env, put, run
+from fabric.api import env
+from fabric.api import put
+from fabric.api import run
 import os.path
 
 
 env.user = 'ubuntu'
 env.hosts = ["18.206.197.184", "54.237.37.228"]
-env.key_filename = "~/.ssh/id_rsa"
+env.key_filename = "~/id_rsa"
 
 
 def do_deploy(archive_path):
     """ Deploys the archive to web servers """
-    if not os.path.exists(archive_path):
+    if os.path.exists(archive_path) is False:
         return False
 
     try:
         archive_name = os.path.basename(archive_path)
         base_name = os.path.splitext(archive_name)[0]
         put(archive_path, '/tmp/')
-        run('mkdir -p /data/web_static/releases/{}/'.format(base_name))
+        sudo('mkdir -p /data/web_static/releases/{}/'.format(base_name))
         main = "/data/web_static/releases/{}".format(base_name)
-        run('tar -xzf /tmp/{} -C {}/'.format(archive_name, main))
-        run('rm /tmp/{}'.format(archive_name))
-        run('mv {}/web_static/* {}/'.format(main, main))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}/ "/data/web_static/current"'.format(main))
+        sudo('tar -xzf /tmp/{} -C {}/'.format(archive_name, main))
+        sudo('rm /tmp/{}'.format(archive_name))
+        sudo('mv {}/web_static/* {}/'.format(main, main))
+        sudo('rm -rf /data/web_static/current')
+        sudo('ln -s {}/ "/data/web_static/current"'.format(main))
         return True
 
-    except Exception as e:
-        print(e)
+    except:
         return False
