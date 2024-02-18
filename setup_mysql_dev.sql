9d2f347eba69bb90cb3381236b7ce188190cd945
aissap <jesus.jesus.core@gmail.com>
2024-02-18 12:23:11 -0800

    Fixing

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,12 @@
+-- Create a database named hbnb_dev_db if it does not already exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- Create a user named hbnb_dev if it does not already exist
 CREATE USER IF NOT EXISTS hbnb_dev@localhost;
+-- Set the password for the hbnb_dev user to 'hbnb_dev_pwd'
 SET PASSWORD FOR hbnb_dev@localhost = 'hbnb_dev_pwd';
+-- Grant all privileges on the hbnb_dev_db database to the hbnb_dev user
 GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO hbnb_dev@localhost;
+-- Grant SELECT privileges on the performance_schema database to the hbnb_dev user
 GRANT SELECT ON performance_schema . * TO hbnb_dev@localhost;
+-- Flush privileges to apply the changes
 FLUSH PRIVILEGES;
